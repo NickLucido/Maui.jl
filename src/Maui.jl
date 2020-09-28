409df5ac0fa6626aad0e160ade961d5d156c0d3f
Nick <nalucido@umich.edu>
2020-09-28 00:28:32 -0400

    Add documenter to deps, remove windows from travis

--- src/Maui.jl
+++ src/Maui.jl
@@ -3,7 +3,7 @@ module Maui
 # This must be edited to work for unix vs. windows
 if Sys.iswindows()
     path = Base.Filesystem.splitpath(@__DIR__)
-    include(joinpath(path[1:end-2]..., "gmsh", "gmsh.jl"))
+    include(joinpath(path[1:end-1]..., "gmsh", "gmsh.jl"))
 else
     using Gmsh
 end
