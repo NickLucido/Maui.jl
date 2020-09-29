35215c49710f4619464b879e11ceffd5a7abf6bb
Nick <nalucido@umich.edu>
2020-09-29 01:51:48 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,22 @@
 using Documenter, Maui
 
-makedocs(sitename="My Documentation")
+pages = [
+    "Home" => "index.md",
+        ],
+]
+
+makedocs(
+   sitename = "FourierFlows.jl",
+    authors = "Nick Lucido",
+    # modules = [Maui],
+     # format = format,
+      pages = pages,
+    doctest = true,
+     # strict = true,
+      clean = true,
+  # checkdocs = :exports
+)
+
 deploydocs(
     repo = "github.com/NickLucido/Maui.jl.git",
 )
